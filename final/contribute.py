ecb549d0cfa35a57c3a3426fd5e4e75e5941f422
Amelia Miner <ameliamminer@gmail.com>
2023-11-29 16:35:58 -0800

    random quote API integrated, contribute.py code improved (wow how did I let that get submitted for hw4)

--- final/contribute.py
+++ final/contribute.py
@@ -9,31 +9,38 @@ from datetime import datetime
 from data_model import get_model
 from lib.quote import Quote
 from lib.date_helper import string_to_date
+from lib.random_quotes_helper import get_random_quote, CATEGORIES
 
 
 class Contribute(MethodView):
 
 
     def get(self):
-        return render_template('contribute.html')
+        return render_template('contribute.html', categories=CATEGORIES)
     
 
     def post(self):
-        quote = request.form['quote']
-        who = request.form['who']
-        if not quote or not who:
-            return render_template('contribute.html', error='Please fill out all required fields')
-        when = request.form['when']
-        where = request.form['where']
-        when = string_to_date(request.form['when'])
-        quote = Quote(
-            quote=request.form['quote'],
-            who=request.form['who'],
-            when=when,
-            where=request.form['where'],
-            how=request.form['how'],
-            context=request.form['context']
-        )
+        if 'lazy' in request.form.keys():
+            category = request.form['category']
+            quote = get_random_quote(category if category != 'random' else None)
+        else:
+            quote_text = request.form['quote']
+            who = request.form['who']
+            if not quote_text or not who:
+                return render_template('contribute.html',
+                    error='Please fill out all required fields')
+            where = request.form['where']
+            when = string_to_date(request.form['when'])
+            how = request.form['how']
+            context = request.form['context']
+            quote = Quote(
+                quote=quote_text,
+                who=who,
+                when=when,
+                where=where,
+                how=how,
+                context=context
+            )
         model = get_model()
         print(f'INFO: inserting {quote}', flush=True) # TODO logging?
         model.insert(quote)
